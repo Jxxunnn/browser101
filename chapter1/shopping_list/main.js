d307493c1cea0ae62e77fff4e53dc29dee37db26
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2021-12-29 13:18:17 +0900

    portfolio updated

--- chapter1/shopping_list/main.js
+++ chapter1/shopping_list/main.js
@@ -1,20 +1,34 @@
 "use strict";
 
-const main = document.querySelector(".main");
-const buttonAdd = document.querySelector(".button__add");
-const ul = document.querySelector(".main__list");
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input')
+const addBtn = document.querySelector('footer__button')
+
+function onAdd() {
+  const newItem = input.ariaValueMax;
+
+}
+
+// 1. 사용자가 입력한
+// 1.사용자가 입력한 텍스트를 받아옴
+// 1.사용자가 입력한 텍스트를 받아옴
+// 2.새로운 아이템을 만듬 (텍스트 + 삭제 버튼)
+// 3. ITEMS 컨테이너 안에 새로 만든 아이템을 추가한다.
+// 4. 인풋을 초기화 한다.
+// 5.인풋을 초기호하ㅏㄴ다
+
 
 buttonAdd.addEventListener("click", () => {
   paintList();
 });
 
-function paintList() {
+function paintList(new) {
   const item = prompt();
   const li = document.createElement("li");
   const span = document.createElement("span");
   span.innerHTML = `${item}`;
   const button = document.createElement("button");
-  button.innerHTML = `<i class="fas fa-trash">`;
+  button.innerHTML = `<i class="fas fa-trash>`;
   li.appendChild(span);
   li.appendChild(button);
   ul.appendChild(li);
