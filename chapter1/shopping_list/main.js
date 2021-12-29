53e2822b298e2c28f11ba8d4d2d0ef714a1284a4
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2021-12-29 15:44:45 +0900

    shopping list refine

--- chapter1/shopping_list/main.js
+++ chapter1/shopping_list/main.js
@@ -1,42 +1,86 @@
 "use strict";
 
-const items = document.querySelector('.items');
-const input = document.querySelector('.footer__input')
-const addBtn = document.querySelector('footer__button')
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
 
 function onAdd() {
-  const newItem = input.ariaValueMax;
+  // 1.사용자가 입력한 텍스트를 받아옴
 
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
+  console.log(text);
+  // 2.새로운 아이템을 만듬 (텍스트 + 삭제 버튼)
+  const item = createItem(text);
+  // 3. ITEMS 컨테이너 안에 새로 만든 아이템을 추가한다.
+  items.appendChild(item);
+  // 4. 새로 추가된 아이템으로 스크롤링
+  item.scrollIntoView({ block: "center" });
+  // 5. 인풋을 초기화 한다.
+  input.value = "";
+  input.focus();
 }
 
-// 1. 사용자가 입력한
-// 1.사용자가 입력한 텍스트를 받아옴
-// 1.사용자가 입력한 텍스트를 받아옴
-// 2.새로운 아이템을 만듬 (텍스트 + 삭제 버튼)
-// 3. ITEMS 컨테이너 안에 새로 만든 아이템을 추가한다.
-// 4. 인풋을 초기화 한다.
-// 5.인풋을 초기호하ㅏㄴ다
+function createItem(text) {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
 
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
 
-buttonAdd.addEventListener("click", () => {
-  paintList();
+  const name = document.createElement("span");
+  name.setAttribute("class", `item__name`);
+  name.innerText = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", `item__delete`);
+  deleteBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+  deleteBtn.addEventListener("click", () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class", "item__divider");
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+input.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    onAdd();
+  }
 });
 
-function paintList(new) {
-  const item = prompt();
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerHTML = `${item}`;
-  const button = document.createElement("button");
-  button.innerHTML = `<i class="fas fa-trash>`;
-  li.appendChild(span);
-  li.appendChild(button);
-  ul.appendChild(li);
+// buttonAdd.addEventListener("click", () => {
+//   paintList();
+// });
 
-  button.addEventListener("click", deleteList);
-}
+// function paintList(new) {
+//   const item = prompt();
+//   const li = document.createElement("li");
+//   const span = document.createElement("span");
+//   span.innerHTML = `${item}`;
+//   const button = document.createElement("button");
+//   button.innerHTML = `<i class="fas fa-trash>`;
+//   li.appendChild(span);
+//   li.appendChild(button);
+//   ul.appendChild(li);
 
-function deleteList(e) {
-  const li = e.target.parentElement.parentElement;
-  li.remove();
-}
+//   button.addEventListener("click", deleteList);
+// }
+
+// function deleteList(e) {
+//   const li = e.target.parentElement.parentElement;
+//   li.remove();
+// }
